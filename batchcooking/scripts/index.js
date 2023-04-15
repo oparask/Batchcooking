cf775734beb7a59bde95fc7ce8fbe3ec4cd5a312
60085@etu.he2b.be <60085@etu.he2b.be>
2023-04-15 14:56:44 +0200

    2.4 commencement

--- batchcooking/scripts/index.js
+++ batchcooking/scripts/index.js
@@ -1,2 +1,32 @@
-console.log("Hello");
+fetch('https://git.esi-bru.be/api/v4/projects/40922/repository/files/recipes.json/raw')
+    .then(response => response.json())
+    .then(loadCards)
+    .catch(alert);
+
+/**let requestURL = 'https://git.esi-bru.be/api/v4/projects/40922/repository/files/recipes.json/raw';
+let request = new XMLHttpRequest();
+request.open('GET', requestURL);
+request.responseType = 'json';
+request.send();
+request.onload = function () {
+    let data = request.response;
+    loadCards(data);
+}
+*/
+
+
+/**
+ * ...
+ * @param {object} data ...
+ */
+function loadCards(data) {
+    const recipes = data;
+    $("#recipe-container h1").text(data["recipes"][0]["recipeName"])
+    console.table(data);
+    
+   
+
+
+}
+
 
